c8781d149eed2ec9fd8bdf999bb799f98c6b827a
Tom Banish <tombanish@gmail.com>
2020-06-11 16:29:32 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,9 @@
 # Create your costume_stores migration here
 
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table costume_stores do |t|
+      
+    end
+  end
+end
